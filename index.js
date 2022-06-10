7d5ef53a32b3661ba4dce548bd95371486a813d4
Maryanne Anyango <mayannanneanyango98@gmail.com>
2022-06-10 21:09:12 +0300

    files

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code in this file!
+
+
+// const currentUser = 'Grace Hopper';
+// const welcomeMessage = 'Welcome to Flatbook, ';
+
+let currentUser = 'Grace Hopper';
+currentUser = currentUser.toUpperCase();
+const welcomeMessage = 'Welcome to Flatbook, '+currentUser+'!';
+
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " +currentUser + "!"
+
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
